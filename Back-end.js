135aa6ccfd49580d21c594001a67e5cc96ad9eed
HegeC5 <hegedavkisvarda2003@gmail.com>
2024-12-05 21:00:56 +0100

    Add files via upload

--- Back-end.js
+++ Back-end.js
@@ -46,29 +46,43 @@ app.post('/api/user/register', async (req, res) => {
     }
 });
 
-// Bejelentkezési végpont
+// Bejelentkezési végpont (minden felhasználóhoz kapcsolódó adatot visszaad)
 app.post('/api/user/login', async (req, res) => {
     const { email, password } = req.body;
 
     try {
+        // Ellenőrizzük, hogy létezik-e az e-mail cím
         const [userResult] = await db.promise().query('SELECT * FROM users WHERE email = ?', [email]);
         if (userResult.length === 0) {
             return res.status(401).json({ message: 'Hibás e-mail cím vagy jelszó.' });
         }
 
         const user = userResult[0];
+
+        // Jelszó érvényesítése
         const isPasswordValid = await bcrypt.compare(password, user.password_hash);
         if (!isPasswordValid) {
             return res.status(401).json({ message: 'Hibás e-mail cím vagy jelszó.' });
         }
 
+        // User kapott értékeléseinek a lekérése
+        const [ratings] = await db.promise().query(
+            'SELECT r.rating_id, r.rating, t.title FROM ratings r ' +
+            'JOIN topics t ON r.topic_id = t.topic_id ' +
+            'WHERE t.user_id = ?', [user.user_id]
+        );
+
+        // User adatok vissza adása
         res.status(200).json({
             message: 'Sikeres bejelentkezés!',
             user: {
-                id: user.id,
+                id: user.user_id,
                 username: user.name,
                 email: user.email,
-                role: user.role
+                role: user.role,
+                profile_picture: user.profile_picture,
+                bio: user.bio,
+                ratings: ratings
             }
         });
     } catch (error) {
@@ -76,6 +90,7 @@ app.post('/api/user/login', async (req, res) => {
         res.status(500).json({ message: 'Hiba történt a bejelentkezés során' });
     }
 });
+
 
 // Keresési végpont
 app.get('/api/topics', async (req, res) => {
